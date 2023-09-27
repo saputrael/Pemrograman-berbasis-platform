3f7a00ba524a96e9d19a08c02241ae39e2dee981
saputrael <119580576+saputrael@users.noreply.github.com>
2023-09-27 13:26:05 +0700

    Add files via upload

---
+++ Biodata.js
@@ -0,0 +1,25 @@
+//Node.js 10.14.0
+//Plain Javascript and Node.js is supported
+// html/css is not supported here 
+// Deklarasi variabel dengan nama dan NIM Anda
+const nama = " Aryo Galuh Saputra";
+const nim = "20220040251";
+
+// Deklarasi variabel dengan tipe data number
+const angka1 = 164;
+const angka2 = 19;
+
+// Melakukan operasi aritmatika sederhana
+const tambah = angka1 + angka2;
+const kurang = angka1 - angka2;
+const kali = angka1 * angka2;
+const bagi = angka1 / angka2;
+
+// Menampilkan identitas dan hasil operasi aritmatika
+console.log(`Nama: ${nama}`);
+console.log(`NIM: ${nim}`);
+console.log(`Hasil Penjumlahan: ${tambah}`);
+console.log(`Hasil Pengurangan: ${kurang}`);
+console.log(`Hasil Perkalian: ${kali}`);
+console.log(`Hasil Pembagian: ${bagi}`);
+console.log("Biodata!")
